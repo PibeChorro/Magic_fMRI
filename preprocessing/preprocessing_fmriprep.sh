c395b8615d904faf2b309a2fd7529a5a8d6c2e92
Vincent Plikat <vincent.plikat@student.uni-tuebingen.de>
2022-04-13 15:01:57 +0200

    restricted number of cpus to 12 and returned from loop to single sub approach

--- preprocessing/preprocessing_fmriprep.sh
+++ preprocessing/preprocessing_fmriprep.sh
@@ -1,35 +1,29 @@
 #!/usr/bin/bash
 
 # Executable bash file to queue on the headnode to run the fmriprep:21.0.1 docker image for preprocessing
-
-
+SUB=${1}
 PROJ_DIR=$HOME/Documents/Magic_fMRI/DATA/MRI
 # define the directory where the raw data is stored
-RAWDATA=$PROJ_DIR/rawdata
 CONTAINER_NAME=vplikat_agbartels_fmriprep_${SUB}
 
-# loop over every subject in RAWDATA and call qsub with docker_fs.sh to reconstruct the subject
-for subject in ${RAWDATA}/sub-*
-do
-    SUB=$(basename "$subject")
-    docker run \
-        --name $CONTAINER_NAME \
-        --interactive \
-        --tty \
-        --rm \
-        --user "$(id -u):$(id -g)" \
-        --volume $PROJ_DIR/rawdata:/data:ro \
-        --volume $PROJ_DIR/derivatives:/out \
-        \
-        nipreps/fmriprep:21.0.1 \
-        --fs-license-file /out/freesurfer/.license \
-        /data /out/fmriprep-21.0.1 \
-        participant --participant-label $SUB \
-        --output-spaces func \
-        --write-graph \
-        --stop-on-first-crash \
-        --fs-no-reconall \
-        --no-submm-recon \
-        --skip_bids_validation \
-        --verbose
-done
+docker run \
+    --name $CONTAINER_NAME \
+    --interactive \
+    --tty \
+    --rm \
+    --user "$(id -u):$(id -g)" \
+    --volume $PROJ_DIR/rawdata:/data:ro \
+    --volume $PROJ_DIR/derivatives:/out \
+    \
+    nipreps/fmriprep:21.0.1 \
+    --fs-license-file /out/freesurfer/.license \
+    /data /out/fmriprep-21.0.1 \
+    participant --participant-label $SUB \
+    --output-spaces func MNI152NLin2009cAsym \
+    --write-graph \
+    --stop-on-first-crash \
+    --fs-no-reconall \
+    --no-submm-recon \
+    --skip_bids_validation \
+    --n_cpus 12 \
+    --verbose
